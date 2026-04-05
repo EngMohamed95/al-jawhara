0ba39b2e8ced4770aa6ab83845dd6203454cb444
Al-Jawhara <admin@al-jawhara.com>
2026-04-05 17:54:06 +0200

    deploy: 2026-04-05 15:54

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const API = process.env.REACT_APP_API_URL || (
   process.env.NODE_ENV === 'production'
-    ? '/api'
+    ? 'http://aljawhara.matix.one:3001'
     : 'http://localhost:3001'
 );
 const h = { 'Content-Type': 'application/json' };
